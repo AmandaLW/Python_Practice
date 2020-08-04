db4176900d476ba39fbf8de2f5b6bb076ec00a44
AmandaLW <mrs.amandalynnward@gmail.com>
2020-08-03 21:05:03 -0700

    vs test

--- practicesheet1.py
+++ practicesheet1.py
@@ -1,2 +1,20 @@
 # these are just files dedicated to practicing core python language concepts and working through python programming challenges
 
+#Factorials with While Loops
+
+# number to find the factorial of
+number = 6
+# start with our product equal to one
+product = 1
+# track the current number being multiplied
+current = 1
+
+while  current <= number:
+    # multiply the product so far by the current number
+    product *= current
+    # increment current with each iteration until it reaches number
+    current += 1
+
+
+# print the factorial of number
+print(product)
